95e18d7bea5068b634bb53017e275844738aab43
Vitorhxt <164032901+Vitorhxt@users.noreply.github.com>
2024-11-12 13:47:06 +0000

    ahvuy

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Jogos',
+    'Valorant e muito melhor que Fortnite?',
+    'Sim mil vezes melhor'
+)
+
+criaCartao(
+    'Musica',
+    'XXXtentacion é melhor que Lil peep?',
+    'Sim com toda certeza'
+)
+
+criaCartao(
+    'Carros',
+    'Maxda RX7 e muito melhor que o R33',
+    'Obiviamente que sim'
+)
+criaCartao(
+    'Motos',
+    'Kawasaki Ninja e muito melhor que honda',
+    'Obiviamente que sim'
+)
